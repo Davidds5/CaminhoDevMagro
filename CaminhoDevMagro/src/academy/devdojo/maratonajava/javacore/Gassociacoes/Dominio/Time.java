f64ee1290becd66472ebfb0b66fcc03f4baafe26
Clovin <fudeoagr12@gmail.com>
2025-10-22 10:20:25 -0300

    Finalizando associações em Java

--- CaminhoDevMagro/src/academy/devdojo/maratonajava/javacore/Gassociacoes/Dominio/Time.java
+++ CaminhoDevMagro/src/academy/devdojo/maratonajava/javacore/Gassociacoes/Dominio/Time.java
@@ -2,17 +2,38 @@ package academy.devdojo.maratonajava.javacore.Gassociacoes.Dominio;
 
 public class Time {
     private String nome;
+    private Jogador[] jogadors;
 
-
-    public Time(String nome) {
+    public Time(String nome){
         this.nome = nome;
     }
+
+    public Time(String nome, Jogador[] jogador) {
+        this.nome = nome;
+        this.jogadors = jogador;
+    }
+
+    public void imprime(){
+        System.out.println(this.nome);
+        if (jogadors == null) return;
+        for (Jogador jogador : jogadors){
+            System.out.println(jogador.getNome());
+        }
+    }
+
+
+
 
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
+    }
+    public Jogador[] getJogadors() {
+        return jogadors;
+    }
+    public void setJogadors(Jogador[] jogadors) {
+        this.jogadors = jogadors;
     }
 }
--- CaminhoDevMagro/src/academy/devdojo/maratonajava/javacore/Gassociacoes/Teste/JogadorTeste01.java
+++ CaminhoDevMagro/src/academy/devdojo/maratonajava/javacore/Gassociacoes/Teste/JogadorTeste01.java
@@ -6,11 +6,18 @@ import academy.devdojo.maratonajava.javacore.Gassociacoes.Dominio.Time;
 public class JogadorTeste01 {
     public static void main(String[] args) {
         Jogador jogador1 = new Jogador("Pele");
-
+        Jogador jogador2 = new Jogador("Messi");
         Time time = new Time("Palmeiras");
+        Jogador[] jogadors = {jogador1, jogador2};
 
+
+        time.setJogadors(jogadors);
         jogador1.setTime(time);
+        jogador2.setTime(time);
+        System.out.println("___JOGADORES___");
         jogador1.imprime();
+        System.out.println("___TIMES___");
+        time.imprime();
 
 
 
