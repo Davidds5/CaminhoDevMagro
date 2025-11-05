5ce82efb7c6960ba387b014e084af03bbdf56fb1
Clovin <fudeoagr12@gmail.com>
2025-11-05 17:34:17 -0300

    Excecoes em java, comeco do aprendizado

---
+++ CaminhoDevMagro/src/academy/devdojo/maratonajava/javacore/Oexeptions/Teste/StackOverFlowTeste01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Oexeptions.Teste;
+
+public class StackOverFlowTeste01 {
+    public static void main(String[] args) {
+        recursividade();
+    }
+    public static void recursividade(){
+        recursividade();
+    }
+}
